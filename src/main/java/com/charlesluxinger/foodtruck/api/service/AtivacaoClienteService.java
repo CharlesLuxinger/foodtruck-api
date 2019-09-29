a8c4a1c627a4cfd6e9f3e2b7c7370c89025b02be
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-29 11:34:00 -0300

    4. Pontos de injeção da anotação @Autowired

--- src/main/java/com/charlesluxinger/foodtruck/api/service/AtivacaoClienteService.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/service/AtivacaoClienteService.java
@@ -8,16 +8,24 @@ import com.charlesluxinger.foodtruck.api.notification.Notificador;
 @Component
 public class AtivacaoClienteService {
 
+	// @Autowired - Pontos de Injeção
 	private Notificador notificador;
 
+	// @Autowired - Pontos de Injeção
+	// Quando há apenas 1 construtor a anotacao nao e necessaria
 	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
+		this.setNotificador(notificador);
 	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
 		this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+
+	// @Autowired - Pontos de Injeção
+	public void setNotificador(Notificador notificador) {
+		this.notificador = notificador;
 	}
 
 }
