01fa2c5a6cc60dba51675f4db0012db5c7b41171
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-29 11:46:03 -0300

    6. Ambiguidade de beans e injeção de lista de beans

--- src/main/java/com/charlesluxinger/foodtruck/api/service/AtivacaoClienteService.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/service/AtivacaoClienteService.java
@@ -1,5 +1,8 @@
 package com.charlesluxinger.foodtruck.api.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.charlesluxinger.foodtruck.api.model.Cliente;
@@ -10,25 +13,26 @@ public class AtivacaoClienteService {
 
 	// @Autowired(required = false) - Pontos de Injeção
 	// Dependencia opcional
-	private Notificador notificador;
+	@Autowired
+	private List<Notificador> notificadores;
 
 	// @Autowired - Pontos de Injeção
 	// Quando há apenas 1 construtor a anotacao nao e necessaria
-	public AtivacaoClienteService(Notificador notificador) {
-		this.setNotificador(notificador);
-	}
+	//	public AtivacaoClienteService(Notificador notificador) {
+	//		this.setNotificador(notificador);
+	//	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		if (notificador != null) {
-			this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		if (notificadores != null) {
+			this.notificadores.forEach(x -> x.notificar(cliente, "Seu cadastro no sistema está ativo!"));
 		}
 	}
 
 	// @Autowired - Pontos de Injeção
-	public void setNotificador(Notificador notificador) {
-		this.notificador = notificador;
-	}
+	// public void setNotificador(Notificador notificador) {
+	//		this.notificador = notificador;
+	// }
 
 }
