c31fb4661a914c139995eef23402547d67bb18c0
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-29 20:50:41 -0300

    17. Criando as tabelas do banco a partir das entidades

---
+++ src/main/java/com/charlesluxinger/foodtruck/api/domain/model/Cozinha.java
@@ -0,0 +1,64 @@
+package com.charlesluxinger.foodtruck.api.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "COZINHAS")
+public class Cozinha {
+
+	@Id
+	@Column(name = "ID")
+	private Integer id;
+
+	@Column(name = "NOME", length = 30)
+	private String nome;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Cozinha other = (Cozinha) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+}
---
+++ src/main/java/com/charlesluxinger/foodtruck/api/domain/model/Restaurante.java
@@ -0,0 +1,76 @@
+package com.charlesluxinger.foodtruck.api.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "RESTAURANTES")
+public class Restaurante {
+	@Id
+	@Column(name = "ID")
+	private Integer id;
+
+	@Column(name = "NOME", length = 30)
+	private String nome;
+
+	@Column(name = "TAXA_FRETE")
+	private BigDecimal taxaFrete;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getTaxaFrete() {
+		return taxaFrete;
+	}
+
+	public void setTaxaFrete(BigDecimal taxaFrete) {
+		this.taxaFrete = taxaFrete;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Restaurante other = (Restaurante) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+}
