c8177397ae87f0fb5f5eda5e62133bab69eb2996
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-30 21:50:12 -0300

    21. Adicionando um objeto no banco de dados

--- src/main/java/com/charlesluxinger/foodtruck/api/jpa/TesteJPA.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/jpa/TesteJPA.java
@@ -5,6 +5,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
 import com.charlesluxinger.foodtruck.api.FoodtruckApiApplication;
+import com.charlesluxinger.foodtruck.api.domain.model.Cozinha;
 
 public class TesteJPA {
 
@@ -16,5 +17,16 @@ public class TesteJPA {
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
 		cadastroCozinha.listar().forEach(x -> System.out.println(x.getNome()));
+
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Brasileira");
+		cadastroCozinha.save(cozinha1);
+
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Japonesa");
+		cadastroCozinha.save(cozinha2);
+
+		cadastroCozinha.listar().forEach(x -> System.out.println(x.getNome()));
+
 	}
 }
