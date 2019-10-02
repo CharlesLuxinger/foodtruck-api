d72be7093c5f2fb7cf8aa91dcc3798620421b99d
Charles Luxinger <charlesluxinger@hotmail.com>
2019-10-01 23:06:57 -0300

    26. Repositório de restaurantes

---
+++ src/main/java/com/charlesluxinger/foodtruck/api/domain/repository/RestauranteRepository.java
@@ -0,0 +1,16 @@
+package com.charlesluxinger.foodtruck.api.domain.repository;
+
+import java.util.List;
+
+import com.charlesluxinger.foodtruck.api.domain.model.Restaurante;
+
+public interface RestauranteRepository {
+
+	public List<Restaurante> findAll();
+
+	public Restaurante findById(Integer id);
+
+	public Restaurante save(Restaurante restaurante);
+
+	public void remove(Restaurante restaurante);
+}
---
+++ src/main/java/com/charlesluxinger/foodtruck/api/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.charlesluxinger.foodtruck.api.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Component;
+
+import com.charlesluxinger.foodtruck.api.domain.model.Restaurante;
+import com.charlesluxinger.foodtruck.api.domain.repository.RestauranteRepository;
+
+@Component
+public class RestauranteRepositoryImpl implements RestauranteRepository {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public List<Restaurante> findAll() {
+		return manager.createQuery("from Restaurante", Restaurante.class).getResultList();
+	}
+
+	@Override
+	public Restaurante findById(Integer id) {
+		return manager.find(Restaurante.class, id);
+	}
+
+
+	@Override
+	@Transactional
+	public Restaurante save(Restaurante restaurante) {
+		return manager.merge(restaurante);
+	}
+
+	@Override
+	@Transactional
+	public void remove(Restaurante restaurante) {
+		Restaurante restauranteFound = findById(restaurante.getId());
+		if (restauranteFound != null) {
+			manager.remove(restauranteFound);
+		}
+	}
+
+}
