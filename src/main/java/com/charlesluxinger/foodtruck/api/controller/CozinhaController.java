a8d3bd7354d016e6ddd773d7e78a1dc1ee674302
Charles Luxinger <charlesluxinger@hotmail.com>
2019-10-03 22:56:22 -0300

    35. Consultando Singleton Resource com GET e @PathVariable

--- src/main/java/com/charlesluxinger/foodtruck/api/controller/CozinhaController.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class CozinhaController {
 	@GetMapping
 	public List<Cozinha> findAll() {
 		return cozinhaRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Cozinha findById(@PathVariable("id") Long id) {
+		return cozinhaRepository.findById(id);
 	}
 }
--- src/main/java/com/charlesluxinger/foodtruck/api/controller/EstadoController.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/controller/EstadoController.java
@@ -3,7 +3,9 @@ package com.charlesluxinger.foodtruck.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,7 @@ import com.charlesluxinger.foodtruck.api.domain.model.Estado;
 import com.charlesluxinger.foodtruck.api.domain.repository.EstadoRepository;
 
 @RestController // Possui ambas anotacoes @ResponseBody @Controller
-@RequestMapping("/estados")
+@RequestMapping(value = "/estados", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 public class EstadoController {
 
 	@Autowired
@@ -20,5 +22,10 @@ public class EstadoController {
 	@GetMapping
 	public List<Estado> findAll() {
 		return estadoRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Estado findById(@PathVariable("id") Long id) {
+		return estadoRepository.findById(id);
 	}
 }
