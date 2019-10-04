417823fb8f3932cc39238607cac2792ca2430ef8
Charles Luxinger <charlesluxinger@hotmail.com>
2019-10-03 22:27:33 -0300

    32. Implementado Controller Cozinhas

--- src/main/java/com/charlesluxinger/foodtruck/api/controller/CozinhaController.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/controller/CozinhaController.java
@@ -1,5 +1,24 @@
 package com.charlesluxinger.foodtruck.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.charlesluxinger.foodtruck.api.domain.model.Cozinha;
+import com.charlesluxinger.foodtruck.api.domain.repository.CozinhaRepository;
+
+@RestController // Possui ambas anotacoes @ResponseBody @Controller
+@RequestMapping("/cozinhas")
 public class CozinhaController {
 
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	@GetMapping
+	public List<Cozinha> findAll() {
+		return cozinhaRepository.findAll();
+	}
 }
