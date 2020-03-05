8294b717bca32c16f8650a266f86d68bb677f949
Charles Luxinger <charlesluxinger@hotmail.com>
2020-03-04 22:03:37 -0300

    54. Externalizando consultas JPQL para um arquivo XML

--- src/main/java/com/charlesluxinger/foodtruck/api/domain/repository/RestauranteRepository.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/domain/repository/RestauranteRepository.java
@@ -4,7 +4,11 @@ import com.charlesluxinger.foodtruck.api.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
+    List<Restaurante> consultarPorNome();
+    
 }
