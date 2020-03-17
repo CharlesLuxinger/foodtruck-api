4ef7bec611496c208db328d58f349d4cd95b8858
Charles Luxinger <charlesluxinger@hotmail.com>
2020-03-16 21:10:52 -0300

    59. Estendendo o JpaRepository para customizar o repositório base

--- src/main/java/com/charlesluxinger/foodtruck/api/FoodtruckApiApplication.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/FoodtruckApiApplication.java
@@ -1,9 +1,12 @@
 package com.charlesluxinger.foodtruck.api;
 
+import com.charlesluxinger.foodtruck.api.domain.repository.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class FoodtruckApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/charlesluxinger/foodtruck/api/domain/repository/RestauranteRepository.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/domain/repository/RestauranteRepository.java
@@ -1,14 +1,13 @@
 package com.charlesluxinger.foodtruck.api.domain.repository;
 
 import com.charlesluxinger.foodtruck.api.domain.model.Restaurante;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>,
+public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>,
                                                 RestauranteRepositoryQueries,
                                                 JpaSpecificationExecutor<Restaurante>{
 
