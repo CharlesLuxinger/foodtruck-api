358bb4c2af5200271bad19a49219bed670f5877d
Charles Luxinger <charles.luxinger@picpay.com>
2020-05-03 20:01:11 -0300

    79. Criando um exception handler global com ResponseEntityExceptionHandler

--- src/main/java/com/charlesluxinger/foodtruck/api/domain/exception/handler/ApiExceptionHandler.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/domain/exception/handler/ApiExceptionHandler.java
@@ -3,49 +3,45 @@ package com.charlesluxinger.foodtruck.api.domain.exception.handler;
 import com.charlesluxinger.foodtruck.api.domain.exception.ConstraintEntityViolationException;
 import com.charlesluxinger.foodtruck.api.domain.exception.DomainException;
 import com.charlesluxinger.foodtruck.api.domain.exception.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice()
-public class ApiExceptionHandler {
-
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> httpMediaTypeNotSupportedExceptionHandler(){
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                             .body(ExceptionResponse.builder()
-                                                    .message("Media Type Supported: application/json")
-                                                    .build()
-                             );
-    }
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> entityNotFoundExceptionHandler(EntityNotFoundException e){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                             .body(ExceptionResponse.builder()
-                                                    .message(e.getMessage())
-                                                    .build()
-                             );
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request){
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(DomainException.class)
-    public ResponseEntity<?> domainExceptionHandler(DomainException e){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                             .body(ExceptionResponse.builder()
-                                    .message(e.getMessage())
-                                    .build()
-                             );
+    public ResponseEntity<?> handleDomainException(DomainException ex, WebRequest request){
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(ConstraintEntityViolationException.class)
-    public ResponseEntity<?> constraintEntityViolationExceptionHandler(ConstraintEntityViolationException e){
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(ExceptionResponse.builder()
-                        .message(e.getMessage())
-                        .build()
-                );
+    public ResponseEntity<?> handleConstraintEntityViolationException(ConstraintEntityViolationException ex, WebRequest request){
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        if (body instanceof String){
+            body = ExceptionResponse.builder()
+                    .message((String) body)
+                    .build();
+        } else if (body == null){
+            body = ExceptionResponse.builder()
+                    .message(status.getReasonPhrase())
+                    .build();
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
 }
--- src/main/java/com/charlesluxinger/foodtruck/api/domain/exception/handler/ExceptionResponse.java
+++ src/main/java/com/charlesluxinger/foodtruck/api/domain/exception/handler/ExceptionResponse.java
@@ -1,5 +1,6 @@
 package com.charlesluxinger.foodtruck.api.domain.exception.handler;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Builder(access = AccessLevel.PACKAGE)
 public class ExceptionResponse {
 
+    @JsonProperty(value = "date_time")
     private final LocalDateTime dateTime = LocalDateTime.now();
 
     private String message;
